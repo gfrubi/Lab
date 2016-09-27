83ef94eb2dbd9a3db8ad63bd056877dd1602339f
Guillermo Rubilar <gfrubi@gmail.com>
2016-09-27 17:16:59 -0300

    nuevo codigo fig. combinaciones suma dados

--- python/fig-combinaciones-suma-dados.py
+++ python/fig-combinaciones-suma-dados.py
@@ -2,12 +2,13 @@
 from matplotlib.pyplot import *
 from numpy import *
 
-x=xrange(2,13)
+x=arange(2,13)
 y=[1,2,3,4,5,6,5,4,3,2,1]
 
 bar(x,y,align="center")
 xlabel("Variable aleatoria, $x$")
 ylabel(u"Número de combinaciones")
-xlim([0,14])
+xlim(0,14)
+ylim(0,7)
 grid()
-savefig("fig-barras-esp-muestral.pdf")
+savefig("../figs/fig-combinaciones-suma-dados.pdf")
